a5a81276f85a1aceb17636fd1f0d790bb644a46e
SergeyLebeDEV <azokblack@gmail.com>
2023-11-20 22:52:57 +0300

    FirstCommit

---
+++ src/test/java/rest/ReqresInTest.java
@@ -0,0 +1,80 @@
+package rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTest {
+
+    @Test
+    void authReqresPost() {
+        // { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+        // { "token": "QpwL5tke4Pnpja7X4" }
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .contentType(ContentType.JSON) //text, picture
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void negativeAuthReqresPost415TypeNo() {
+
+        given()
+                .log().uri()
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(415);
+    }
+
+    @Test
+    void negativeAuthReqresPost400() {
+
+        given()
+                .log().uri()
+                .body("Bad")
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void authReqresPostMissingPassword() {
+        // { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+        // { "token": "QpwL5tke4Pnpja7X4" }
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .contentType(ContentType.JSON) //text, picture
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
---
+++ src/test/java/rest/SelenoidApiTest.java
@@ -0,0 +1,144 @@
+package rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SelenoidApiTest {
+
+    @Test
+    void selenoidRestRequest() {
+
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20))
+                //     ||
+                .assertThat().body("total", equalTo(20));
+
+    }
+
+    @Test
+    void selenoidRestRequestGiven() {
+
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+
+    }
+
+    @Test
+    void selenoidRestRequestGivenLog() {
+
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("total", is(20));
+
+    }
+
+    @Test
+    void selenoidRestRequestGivenLogs() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+
+    }
+
+    @Test
+    void selenoidRestRequestVersionChrome() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("browsers", hasKey("firefox"))
+                .body("browsers.chrome", hasKey("100.0"));
+
+    }
+
+    @Test
+    void selenoidRestRequestGoodTotalPath() {
+        Integer expectedTotal = 20;
+
+        Integer actualTotal = given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+
+                .then()
+                .log().status()
+                .statusCode(200)
+                .extract().path("total");
+        assertEquals(expectedTotal, actualTotal);
+
+    }
+
+
+    @Test
+    void checkHubNoAuth() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(401);
+    }
+
+    @Test
+    void checkHubAuth200() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+
+    @Test
+    void checkHubAuth200Wd() {
+        given()
+                .log().uri()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+
+}
